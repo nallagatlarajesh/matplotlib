cc24077b67b30d5ea66ca7ef4fef9a6a3e2e0812
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-01-30 16:31:52 +0530

    Create matpotlib2.py

---
+++ matpotlib2.py
@@ -0,0 +1,10 @@
+#first array x-axis
+#second array y-axis
+from matplotlib import pyplot as plt
+x=[4,2,3]
+y=[2,10,3]
+plt.plot(x,y)
+plt.title("info")
+plt.ylabel("Y axis")
+plt.xlabel("X axis")
+plt.show()
